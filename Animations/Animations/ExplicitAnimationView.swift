e80b168a6b5b627104a2fc6c21f5cd9d19017973
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-23 06:30:53 +0200

    add spin around y

--- Animations/Animations/ExplicitAnimationView.swift
+++ Animations/Animations/ExplicitAnimationView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ExplicitAnimationView: View {
+    
+    @State private var animationAmount = 0.0
+    
     var body: some View {
         Button("Tap me") {
-            
+            withAnimation {
+                animationAmount += 360
+            }
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(.circle)
+        .rotation3DEffect(
+            .degrees(animationAmount),
+            axis: (x: 0.0, y: 1.0, z: 0.0)
+        )
     }
 }
 
