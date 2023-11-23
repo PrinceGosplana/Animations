2c70a12583436cceb5de913e4b223fd0759ad55c
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-23 06:25:53 +0200

    add initial button

--- Animations/Animations/ExplicitAnimationView.swift
+++ Animations/Animations/ExplicitAnimationView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ExplicitAnimationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Tap me") {
+            
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(.circle)
     }
 }
 
