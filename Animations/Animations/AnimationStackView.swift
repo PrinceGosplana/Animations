118393889e595509b6cfb2eb37a5be1a127fe05a
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-23 06:54:55 +0200

    add initial view for stack animation

---
+++ Animations/Animations/AnimationStackView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationStackView.swift
+//  Animations
+//
+//  Created by OLEKSANDR ISAIEV on 23.11.2023.
+//
+
+import SwiftUI
+
+struct AnimationStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationStackView()
+}
