80063b84e8ea2f599a396339a7d7951ab762b475
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-23 06:24:39 +0200

    add new view for explicit animation

---
+++ Animations/Animations/ExplicitAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  ExplicitAnimationView.swift
+//  Animations
+//
+//  Created by OLEKSANDR ISAIEV on 23.11.2023.
+//
+
+import SwiftUI
+
+struct ExplicitAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExplicitAnimationView()
+}
