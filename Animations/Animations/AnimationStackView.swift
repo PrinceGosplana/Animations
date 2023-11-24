d35d692529900da9a5bbfe8892216cc0534f48e9
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-24 06:11:03 +0200

    order of modifiers does matter

--- Animations/Animations/AnimationStackView.swift
+++ Animations/Animations/AnimationStackView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct AnimationStackView: View {
+    
+    @State private var enabled = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Tap me") {
+            enabled.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .foregroundStyle(.white)
+        .animation(.default, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
     }
 }
 
