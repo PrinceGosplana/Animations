bc6389a51c6c865edcf12115b22d4d1b64aa3655
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-25 13:34:44 +0200

    nice scaling animation

--- Animations/Animations/VisibilityView.swift
+++ Animations/Animations/VisibilityView.swift
@@ -22,6 +22,7 @@ struct VisibilityView: View {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
+                    .transition(.scale)
             }
         }
     }
