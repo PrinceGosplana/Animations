694207eb2757158acc36a92661bd88dd1f7474f0
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-25 07:20:06 +0200

    add animating gestures view

---
+++ Animations/Animations/AnimatingGesturesView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatingGesturesView.swift
+//  Animations
+//
+//  Created by OLEKSANDR ISAIEV on 25.11.2023.
+//
+
+import SwiftUI
+
+struct AnimatingGesturesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatingGesturesView()
+}
