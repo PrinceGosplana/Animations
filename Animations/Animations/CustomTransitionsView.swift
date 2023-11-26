5fc5ff976a9c87870d2b0d8e8547a44165caa04f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-26 05:45:18 +0200

    add new view for custom transitions using viewmodifier

---
+++ Animations/Animations/CustomTransitionsView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTransitionsView.swift
+//  Animations
+//
+//  Created by OLEKSANDR ISAIEV on 26.11.2023.
+//
+
+import SwiftUI
+
+struct CustomTransitionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTransitionsView()
+}
